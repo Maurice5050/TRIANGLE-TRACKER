6c97d4197aab38ad0a095ed8a63b5d03f8c98282
mkoyandhe@gmail.com <mkoyandhe@gmail.com>
2019-02-15 17:39:09 +0300

    modified index.html

---
+++ traingle.js
@@ -0,0 +1,6 @@
+function jack(){
+  var sideone = parseInt(document.getElementById('side-1').value);
+  var sidetwo = parseInt(document.getElementById('side-2').value);
+  var sidethree = parseInt(document.getElementById('side-3').value);
+}
+var = side (sideone,siadetwo,sidethree)
