8be9ea29244e165837e98a44d8104bd9c9a12b39
mkoyandhe@gmail.com <mkoyandhe@gmail.com>
2019-02-15 18:00:54 +0300

    modified js file

--- traingle.js
+++ traingle.js
@@ -3,4 +3,7 @@ function jack(){
   var sidetwo = parseInt(document.getElementById('side-2').value);
   var sidethree = parseInt(document.getElementById('side-3').value);
 }
-var = side (sideone,siadetwo,sidethree)
+var = side =[sideone,sidetwo,sidethree];
+if ( side [0] + side[1] <= side[0] || side[0] + side[2] <= side[1] || side[2] + side[1] <= side[0]]){
+  alert()
+}
