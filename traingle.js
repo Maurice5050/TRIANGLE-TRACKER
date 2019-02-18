3bcdb7eade09463950ab543105d89efb8d4f95fc
mkoyandhe@gmail.com <mkoyandhe@gmail.com>
2019-02-18 10:46:48 +0300

    added image

--- traingle.js
+++ traingle.js
@@ -3,7 +3,19 @@ function jack(){
   var sidetwo = parseInt(document.getElementById('side-2').value);
   var sidethree = parseInt(document.getElementById('side-3').value);
 }
-var = side =[sideone,sidetwo,sidethree];
-if ( side side[0] + side[1] <= side[0] || side[0] + side[2] <= side[1] || side[2] + side[1] <= side[0]]){
-  alert()
+var side =[sideone,sidetwo,sidethree];
+if (side[0] + side[1] <= side[2] || side[1] + side[2] <= side[0] || side[2] + side[0] <= side[1]){
+
 }
+if side[0]== side[1] $$ side[1] == side[2] $$ side[2] == side[0]){
+  alert("This is an Equilateral");
+}
+else if (side[0]!== side[1] $$ side[1]!== side[2] $$ side[2]!== side[0]){
+alert("This is scalene");
+}
+else {
+  alert("This is an Issosceles");
+}
+else{
+  alert("This is not a triangle");
+{
