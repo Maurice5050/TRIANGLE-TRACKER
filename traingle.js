6a93a08f9c1ba6d4dfe5efbc5f54b56dac71bd1c
Maurice5050 <47355420+Maurice5050@users.noreply.github.com>
2019-03-01 10:19:01 +0300

    Added Updated Html,js, css files

--- traingle.js
+++ traingle.js
@@ -1,21 +1,34 @@
-function jack(){
-  var sideone = parseInt(document.getElementById('side-1').value);
-  var sidetwo = parseInt(document.getElementById('side-2').value);
-  var sidethree = parseInt(document.getElementById('side-3').value);
-}
-var side =[sideone,sidetwo,sidethree];
-if (side[0] + side[1] <= side[2] || side[1] + side[2] <= side[0] || side[2] + side[0] <= side[1]){
 
-}
-if side[0]== side[1] $$ side[1] == side[2] $$ side[2] == side[0]){
-  alert("This is an Equilateral");
-}
-else if (side[0]!== side[1] $$ side[1]!== side[2] $$ side[2]!== side[0]){
-alert("This is scalene");
-}
-else {
-  alert("This is an Issosceles");
-}
-else{
-  alert("This is not a triangle");
-{
+
+         var tracker = function(sideone, sidetwo, sidethree) {
+
+           var sideone= parseInt(document.getElementById("side-1").value);
+           var sidetwo = parseInt(document.getElementById("side-2").value);
+           var sidethree = parseInt(document.getElementById("side-3").value);
+
+           var sides = [];
+           sides.push(sideone);
+           sides.push(sidetwo);
+           sides.push(sidethree);
+
+           if ((sideone + sidetwo> sidethree && sideone + sidethree > sidetwo && sidetwo + sidethree > sideone)
+           && (sideone=== sidetwo && sidetwo === sidethree)) {
+           document.getElementById("results").innerHTML = "Its an Equilateral Triangle <br> " ;
+
+           } else if ((sideone + sidetwo > sidethree && sideone + sidethree > sidetwo && sidetwo + sidethree > sideone)
+            && ((sideone === sidetwo && sidetwo !== sidethree) ||
+           (sideone === sidethree && sidethree !== sidetwo) || (sidetwo=== sidethree && sidetwo !== sideone))) {
+             document.getElementById("results").innerHTML = "Its an Isoscele Triangle <br> " ;
+
+           } else if ((sideone + sidetwo > sidethree && sideone + sidethree > sidetwo && sidetwo + sidethree > sideone)
+            && (sideone !== sidetwo && sidetwo !== sidethree && sideone !== sidethree)) {
+             document.getElementById("results").innerHTML = "Its a Scalene Triangle <br> " ;
+
+           } else if (!sideone || !sidetwo || !sidethree) {
+             document.getElementById("results").innerHTML = "Enter all the sides and try again";
+           } else {
+             document.getElementById("results").innerHTML = " It is not a triangle!";
+
+
+           }
+         }
